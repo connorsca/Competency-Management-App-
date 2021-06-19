76e8a2ec5d84ef0e832b6669965311d3a9d91e69
Carl Connors <connorsca@hotmail.co.uk>
2021-06-19 21:20:31 +0100

    add all pages with routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,40 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { PersonalInfoComponent } from './home/personal-info/personal-info.component';
+import { TeamInfoComponent } from './home/team-info/team-info.component';
+import { StatsComponent } from './home/stats/stats.component';
+import { AddComponent } from './home/add/add.component';
+import { UploadComponent } from './home/upload/upload.component';
+import { DownloadComponent } from './home/download/download.component';
+
+const appRoutes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'personal-info', component: PersonalInfoComponent },
+  { path: 'team-memebrs', component: TeamInfoComponent },
+  { path: 'team-stats', component: StatsComponent },
+  { path: 'add', component: AddComponent },
+  { path: 'upload', component: UploadComponent },
+  { path: 'download', component: DownloadComponent },
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    PersonalInfoComponent,
+    TeamInfoComponent,
+    StatsComponent,
+    AddComponent,
+    UploadComponent,
+    DownloadComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
