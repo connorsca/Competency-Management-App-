d6dbdeeb23dd258bbc36e732c3739322041c8107
Carl Connors <connorsca@hotmail.co.uk>
2021-07-23 19:53:48 +0100

    add statistics page functions

--- src/app/data.service.ts
+++ src/app/data.service.ts
@@ -13,9 +13,9 @@ export class DataService {
   userQualInfo = [
     'Personal Information',
     0,
-    { qual: 'Human Factors', exp: '01/02/2021' },
+    { qual: 'Human Factors', exp: '01/08/2021' },
     { qual: 'DIMP', exp: '01/05/2021' },
-    { qual: 'Manual Handling', exp: '01/05/2023' },
+    { qual: 'Manual Handling', exp: '20/08/2021' },
     { qual: 'Noise Awareness', exp: '01/07/2022' },
     { qual: 'Dangerous Substances', exp: '01/05/2025' },
   ];
@@ -35,7 +35,7 @@ export class DataService {
       0,
       { qual: 'Human Factors', exp: '29/05/2020' },
       { qual: 'DIMP', exp: '19/05/2021' },
-      { qual: 'Manual Handling', exp: '01/05/2023' },
+      { qual: 'Manual Handling', exp: '20/08/2021' },
       { qual: 'Noise Awareness', exp: '01/07/2022' },
       { qual: 'Dangerous Substances', exp: '01/05/2025' },
     ],
@@ -139,5 +139,71 @@ export class DataService {
   setupNewTeamMem() {
     var i = this.teamQualInfo.length;
     this.setSelectedQualInfo(this.teamQualInfo[i - 1]);
+  }
+
+  // Statisitcs functions
+  setStats() {
+    let totalOod = 0;
+    let totalQuals = 0;
+    let totalInDate = 0;
+    let totalOodThirtyDays = 0;
+    this.oodTeamQuals();
+    // Amount in team
+    var totalTeamMem = this.teamQualInfo.length;
+
+    // OOD
+    for (var i = 0; i < this.teamQualInfo.length; i++) {
+      var teamMem = this.teamQualInfo[i];
+      var teamMemOod = teamMem[1];
+      totalOod += Number(teamMemOod);
+    }
+
+    // in date
+    for (var i = 0; i < this.teamQualInfo.length; i++) {
+      var teamMem = this.teamQualInfo[i];
+      var totalTeamMemQuals = teamMem.length - 2;
+      totalQuals += Number(totalTeamMemQuals);
+    }
+    totalInDate = totalQuals - totalOod;
+
+    // OOD in 30 days
+    this.oodInThirtyDays();
+    totalOodThirtyDays = this.oodInThirtyDays();
+
+    return [totalTeamMem, totalOod, totalInDate, totalOodThirtyDays];
+  }
+
+  oodInThirtyDays() {
+    let today = new Date();
+    let oneMonth = new Date(
+      today.getFullYear(),
+      today.getMonth() + 1,
+      today.getDate()
+    );
+    let totalOod = 0;
+
+    this.teamQualInfo.forEach((teamMember) => {
+      teamMember.slice(1).forEach((teamMemberQual) => {
+        Object.keys(teamMemberQual).forEach(function (key, index) {
+          if (key === 'exp') {
+            let dateString = Object.values(teamMemberQual)[index];
+            let formatDateString =
+              dateString.substr(6, 4) +
+              ', ' +
+              dateString.substr(3, 2) +
+              ', ' +
+              dateString.substr(0, 2);
+            let convertDateString = new Date(formatDateString);
+            if (
+              convertDateString.getTime() >= today.getTime() &&
+              convertDateString.getTime() <= oneMonth.getTime()
+            ) {
+              totalOod++;
+            }
+          }
+        });
+      });
+    });
+    return totalOod;
   }
 }
--- src/app/home/stats/stats.component.ts
+++ src/app/home/stats/stats.component.ts
@@ -1,15 +1,29 @@
 import { Component, OnInit } from '@angular/core';
+import { DataService } from 'src/app/data.service';
 
 @Component({
   selector: 'app-stats',
   templateUrl: './stats.component.html',
-  styleUrls: ['./stats.component.css']
+  styleUrls: ['./stats.component.css'],
 })
 export class StatsComponent implements OnInit {
-
-  constructor() { }
+  constructor(private data: DataService) {}
 
   ngOnInit(): void {
+    this.getStats();
   }
 
+  totalTeamMem = 0
+  totalOod = 0
+  totalInDate = 0
+  totalOodThirtyDays = 0
+
+
+  getStats() {
+    let returnedStats = this.data.setStats();
+    this.totalTeamMem = returnedStats[0]
+    this.totalOod = returnedStats[1]
+    this.totalInDate = returnedStats[2]
+    this.totalOodThirtyDays = returnedStats[3]
+  }
 }
